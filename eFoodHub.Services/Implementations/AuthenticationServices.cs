71bbe0d5287a0c2d4e36e77a3742b68fddeea608
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-06 13:45:04 +0300

    Creating authentication service

---
+++ eFoodHub.Services/Implementations/AuthenticationServices.cs
@@ -0,0 +1,72 @@
+﻿using eFoodHub.Entities;
+using eFoodHub.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFoodHub.Services.Implementations
+{
+    public class AuthenticationServices : IAuthenticationServices
+    {
+        protected SignInManager<User> _signManager;
+        protected UserManager<User> _userManager;
+        protected RoleManager<Role> _roleManager;
+
+        public AuthenticationServices(SignInManager<User> signManager,UserManager<User> userManager,RoleManager<Role> roleManager)
+        {
+            signManager = _signManager;
+            userManager = _userManager;
+            roleManager = _roleManager;
+        }
+
+        public User AuthenticateUser(string Username, string Password)
+        {
+            var result = _signManager.PasswordSignInAsync(Username, Password, false, lockoutOnFailure: false).Result;
+            if (result.Succeeded)
+            {
+                var user = _userManager.FindByNameAsync(Username).Result;
+                var roles = _userManager.GetRolesAsync(user).Result;
+                user.Roles = roles.ToArray();
+                return user;
+            }
+            return null;
+        }
+
+        public bool CreateUser(User user, string Password)
+        {
+            var result = _userManager.CreateAsync(user, Password).Result;
+            if (result.Succeeded)
+            {
+                // a u
+                string role = "Admin";
+                var res = _userManager.AddToRoleAsync(user, role).Result;
+                if (res.Succeeded)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public User GetUser(string Username)
+        {
+            return _userManager.FindByNameAsync(Username).Result;
+        }
+
+        public async Task<bool> SignOut()
+        {
+            try
+            {
+                await _signManager.SignOutAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ eFoodHub.Services/Interfaces/IAuthenticationServices.cs
@@ -0,0 +1,18 @@
+﻿using eFoodHub.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFoodHub.Services.Interfaces
+{
+    public interface IAuthenticationServices
+    {
+        bool CreateUser(User user, string Password);
+        Task<bool> SignOut();
+        User AuthenticateUser(string Username, string Password);
+        User GetUser(string Username);
+    
+    }
+}
