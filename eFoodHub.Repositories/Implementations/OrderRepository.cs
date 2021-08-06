691ddaa253b9a8deb61e233abc031fa44620ba45
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-06 11:22:42 +0300

    Creating order repository

---
+++ eFoodHub.Repositories/Implementations/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using eFoodHub.Entities;
+using eFoodHub.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFoodHub.Repositories.Implementations
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        private AppDbContext appContext
+        {
+            get
+            {
+                return _dbContext as AppDbContext;
+            }
+        }
+
+        public OrderRepository(DbContext dbContext) : base(dbContext)
+        {
+
+        }
+        public IEnumerable<Order> GetUsersOrder(int userId)
+        {
+            return appContext.Orders
+
+                .Include(o => o.OrderItems)
+                .Where(x => x.UserId == userId).ToList();
+        }
+    }
+}
---
+++ eFoodHub.Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using eFoodHub.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFoodHub.Repositories.Interfaces
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetUsersOrder(int userId);
+
+    }
+}
