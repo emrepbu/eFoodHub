27635fce01c11fbb090359f3f8dd60b1d7a8a33d
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-06 15:11:18 +0300

    Implementing user sign up

--- eFoodHub.Repositories/SQLScripts/SeedData.sql
+++ eFoodHub.Repositories/SQLScripts/SeedData.sql
@@ -23,3 +23,6 @@ SET IDENTITY_INSERT [dbo].[AspNetRoles] ON
 GO
 INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (1, N'Admin', N'ADMIN', N'979797')
 GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (2, N'User', N'USER', N'979799')
+GO
+SET IDENTITY_INSERT [dbo].[AspNetRoles] OFF
---
+++ eFoodHub.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using eFoodHub.Entities;
+using eFoodHub.Services.Interfaces;
+using eFoodHub.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFoodHub.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthenticationServices _authServices;
+
+        public AccountController(IAuthenticationServices authServices)
+        {
+            _authServices = authServices;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                };
+                bool result = _authServices.CreateUser(user, model.Password);
+                if (result)
+                {
+                    RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+    }
+}
